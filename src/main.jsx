a61bfb2b26e4ba8c395c5cee245fa0ce47e85cce
Braian Palacios <braianpalacios699@gmail.com>
2025-02-14 20:57:56 -0300

    update path context

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './style.css';
-import { FilterContextProvider } from './context/filterContext';
+import { FilterContextProvider } from './context/FilterContext';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
