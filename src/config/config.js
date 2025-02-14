9d210bf00d91e9c94eda78224e0ac9265458300d
Braian Palacios <braianpalacios699@gmail.com>
2025-02-14 20:40:13 -0300

    configuration issue resolved

--- src/config/config.js
+++ src/config/config.js
@@ -2,8 +2,8 @@ export const configUrlAPI = ({toMoney, fromMoney, amount })=>{
   const today = new Date();
   const formatDate = today.toISOString().split('T');
   return {
-    URL_API :'https://api.apilayer.com/',
-    PATH_CONVERT:'exchangerates_data/convert?',
+    URL_API :import.meta.env.VITE_URL_API,
+    PATH_CONVERT:import.meta.env.VITE_PATH_CONVERT,
     TO: `to=${toMoney}&`,
     FROM:  `from=${fromMoney}&`,
     AMOUNT:`amount=${amount}&`,
@@ -14,7 +14,7 @@ export const configUrlAPI = ({toMoney, fromMoney, amount })=>{
 export const configApiHead = ()=>{
   return {
     headers: {
-      'apikey': '6rSfq72fag0ERNmVZQQZY4HAmBrgRzLA',
+      'apikey': import.meta.env.VITE_API_KEY,
     }
   };
 };
